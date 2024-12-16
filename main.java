6aab19731eb6e9b8630553daa6551bfe8a199879
advitdhaon <167999205+advitdhaon@users.noreply.github.com>
2024-12-16 22:50:59 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,16 @@
+public class Perfection {
+    // QGUJJIGQNY KU VJG GPIAF QH EQPGXKOHQ. ULRPNEKDWA KU C FXVJIQ J DJFW EPQPI.
+    public static void main(String args[]) {
+
+        System.out.println("");
+    }
+
+    public static String evaluateLife(String perfection, String contenment) {
+        if (perfection.equals("failed")) {
+            return "Perfection? A useless Concept.";
+        } else if (perfection.equals("achieved")) {
+            return "Perfection? Maybe it doesn't even matter";
+        }
+        return "KVISZKH YOZMW DZH ZOO ZNRHS MVVWVW";
+    }
+}
